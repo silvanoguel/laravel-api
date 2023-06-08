f65ba06e15c04e06ce5a8109fd0f9d130e622ee7
silvanoguel <silvanoguel@msn.com>
2023-06-08 15:43:06 +0200

    add:technology section in show page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,18 +1,38 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1 class="text-center">{{ $project->title }}</h1>
-    <div class="d-flex justify-content-between">
+    <h1 class="text-center my-4">{{ $project->title }}</h1>
+    <div class="my-4">
         @if ($project->type)
-            <span>Type: {{ $project->type->name }}</span>
+            <h5>Type:</h5>
+            <span>{{ $project->type->name }}</span>
         @else
             <span>No category</span>
         @endif
-        {{ $project->slug }}
+      
     </div>
-    <p class="mt-4">{{ $project->content }}</p>
+
+    <div class="my-4">
+        <h5>Technologies: </h5>
+        @forelse ($project->technologies as $technology)
+            <span>{{ $technology->name }} {{ $loop->last ? '' : ',' }}</span>
+        @empty
+            <span>No technology</span>
+        @endforelse
+    </div>
+
+    <div class="my-5">
+        <h5>Content:</h5>
+        <p class="mt-4">{{ $project->content }}</p>
+    </div>
+   
 
     <div class="my-3">
         <a class="btn btn-primary" href="{{ route('admin.projects.index') }}">Go back to Project list</a>
+    </div>  
+    
+    <div class="my-5 text-end">
+        <span>{{ $project->slug }}</span>
     </div>
+    
 @endsection
