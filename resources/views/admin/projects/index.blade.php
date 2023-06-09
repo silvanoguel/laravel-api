8a6e11c4fce0750dc994df3ea31eea9e1694252d
silvanoguel <silvanoguel@msn.com>
2023-06-09 15:18:41 +0200

    add: technologies in index page

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,48 +3,54 @@
 @section('content')
     <h1>Project list</h1>
 
-        @include('partials.session_message')
+    @include('partials.session_message')
 
 
-        <a href="{{ route('admin.projects.create') }}" class="btn btn-primary">Create a new project</a>
+    <a href="{{ route('admin.projects.create') }}" class="btn btn-primary">Create a new project</a>
 
 
-        <table class="table">
-            <thead>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Type</th>
+                <th scope="col">Technology</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
                 <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Slug</th>
-                    <th scope="col">Actions</th>
+                    <th scope="row">{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->type?->name }}</td>
+                    <td>
+                        @foreach ($project->technologies as $technology)
+                            <span>{{ $technology->name }} {{ $loop->last ? '' : ',' }}</span>
+                        @endforeach
+                    </td>
+                    <td>{{ $project->slug }}</td>
+                    <td>
+                        <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-success">
+                            <i class="fa-solid fa-eye"></i>
+                        </a>
+                        <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-warning">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                        </a>
+                        <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->slug) }}"
+                            method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($projects as $project)
-                    <tr>
-                        <th scope="row">{{ $project->id }}</th>
-                        <td>{{ $project->title }}</td>
-                        <td>{{ $project->type?->name }}</td>
-                        <td>{{ $project->slug }}</td>
-                        <td>
-                            <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-success">
-                                <i class="fa-solid fa-eye"></i>
-                            </a>
-                            <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-warning">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </a>
-                            <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->slug) }}"
-                                method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">
-                                    <i class="fa-solid fa-trash"></i>
-                                </button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
+            @endforeach
 
-            </tbody>
-        </table>
-    @endsection
+        </tbody>
+    </table>
+@endsection
